e3c3efe5dbcf521c1fc1e9228d3a69ab28a938df
elham34 <elham.alwadie@gmail.com>
2023-10-15 11:29:46 +0300

    Initial Commit

---
+++ PROJECT/PROJECTApp.swift
@@ -0,0 +1,17 @@
+//
+//  PROJECTApp.swift
+//  PROJECT
+//
+//  Created by Elham on 30/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct PROJECTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
